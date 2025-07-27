62e3a13c74f0442bec7c949adb35d976c0fee7e5
ali-ahadi1105 <aliahadi.f1105@gmail.com>
2025-07-27 20:40:37 +0330

    add changes

---
+++ catalog_service/src/models/Product.model.ts
@@ -0,0 +1,9 @@
+export class Product {
+    constructor(
+        public readonly name: string,
+        public readonly description: string,
+        public readonly price: string,
+        public readonly stock: string,
+        public readonly id?: string,
+    ) {}
+}
